e9fcf65f7da0f38aaff4bc0cb23eea083daa6970
Adam Fast <adam@adamfast.com>
2012-03-12 14:46:28 -0500

    PEP8 / whitespace formatting fixes

--- tigerline/admin.py
+++ tigerline/admin.py
@@ -1,12 +1,16 @@
 from django.contrib.gis import admin
-from models import Zipcode, State, County
+
+from tigerline.models import Zipcode, State, County
+
 
 class ZipcodeAdmin(admin.OSMGeoAdmin):
     list_display = ('code',)
     search_fields = ('code',)
 
+
 class StateAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'usps_code', 'fips_code', 'area_description_code', 'feature_class_code', 'functional_status')
+
 
 class CountyAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'county_identifier', 'legal_statistical_description', 'fips_55_class_code', 'feature_class_code', 'functional_status')
--- tigerline/views.py
+++ tigerline/views.py
@@ -4,6 +4,7 @@ from django.contrib.gis.maps.google.overlays import GMarker, GEvent, GPolyline, 
 from django.shortcuts import get_object_or_404, render_to_response
 from models import Zipcode
 
+
 def zipcode_detail(request, object_id):
     zipcode = get_object_or_404(Zipcode, code=object_id)
 
